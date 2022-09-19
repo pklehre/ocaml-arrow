b5ebda1766a803cce4a613a66e8283d14fbfee1f
pklehre <p.k.lehre@cs.bham.ac.uk>
2022-09-19 21:07:21 +0100

    .

--- src/wrapper/table.ml
+++ src/wrapper/table.ml
@@ -11,6 +11,79 @@ type _ col_type =
   | Ofday_ns : Core.Time_ns.Ofday.t col_type
   | Bool : bool col_type
 
+
+type packed_col =
+  | P : 'a col_type * 'a array -> packed_col
+  | O : 'a col_type * 'a option array -> packed_col
+
+let col (type a) (data : a array) (type_ : a col_type) ~name =
+  let open Writer in
+  match type_ with
+  | Int -> int data ~name
+  | Float -> float data ~name
+  | Utf8 -> utf8 data ~name
+  | Date -> date data ~name
+  | Time_ns -> time_ns data ~name
+  | Span_ns -> span_ns data ~name
+  | Ofday_ns -> ofday_ns data ~name
+  | Bool ->
+    let bs = Valid.create_all_valid (Array.length data) in
+    Array.iteri data ~f:(fun i a -> if not a then Valid.set bs i a);
+    bitset bs ~name
+
+let col_opt (type a) (data : a option array) (type_ : a col_type) ~name =
+  let open Writer in
+  match type_ with
+  | Int -> int_opt data ~name
+  | Float -> float_opt data ~name
+  | Utf8 -> utf8_opt data ~name
+  | Date -> date_opt data ~name
+  | Time_ns -> time_ns_opt data ~name
+  | Span_ns -> span_ns_opt data ~name
+  | Ofday_ns -> ofday_ns_opt data ~name
+  | Bool ->
+    let bs = Valid.create_all_valid (Array.length data) in
+    let valid = Valid.create_all_valid (Array.length data) in
+    Array.iteri data ~f:(fun i a ->
+        match a with
+        | None -> Valid.set valid i false
+        | Some false -> Valid.set bs i false
+        | Some true -> ());
+    bitset_opt bs ~valid ~name
+
+let named_col packed_col ~name =
+  match packed_col with
+  | P (typ_, data) -> col data typ_ ~name
+  | O (typ_, data) -> col_opt data typ_ ~name
+
+let create cols = Writer.create_table ~cols
+
+let read (type a) t ~column (col_type : a col_type) : a array =
+  match col_type with
+  | Int -> Wrapper.Column.read_int t ~column
+  | Float -> Wrapper.Column.read_float t ~column
+  | Utf8 -> Wrapper.Column.read_utf8 t ~column
+  | Date ->
+    (try Wrapper.Column.read_date t ~column with
+    | _ -> Wrapper.Column.read_utf8 t ~column |> Array.map ~f:Core.Date.of_string)
+  | Time_ns ->
+    (try Wrapper.Column.read_time_ns t ~column with
+    | _ ->
+      Wrapper.Column.read_utf8 t ~column |> Array.map ~f:Core.Time_ns.of_string_with_utc_offset)
+  | Span_ns ->
+    (try Wrapper.Column.read_span_ns t ~column with
+    | _ ->
+      Wrapper.Column.read_utf8 t ~column
+      |> Array.map ~f:Core.Time_ns.Span.of_string)
+  | Ofday_ns ->
+    (try Wrapper.Column.read_ofday_ns t ~column with
+    | _ ->
+      Wrapper.Column.read_utf8 t ~column
+      |> Array.map ~f:Core.Time_ns.Ofday.of_string)
+  | Bool ->
+    let bs = Wrapper.Column.read_bitset t ~column in
+    Array.init (Valid.length bs) ~f:(Valid.get bs)
+
 let read_opt (type a) t ~column (col_type : a col_type) : a option array =
   match col_type with
   | Int -> Wrapper.Column.read_int_opt t ~column
